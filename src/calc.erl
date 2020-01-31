891fcf633dd92ad9b7000896a7836cfdacad07ec
Artur-Yurii Korchynskyi <ext-artur-yurii.korchynskyi@here.com>
2020-01-31 02:40:23 +0200

    Started working on converting from standart to rpn

--- src/calc.erl
+++ src/calc.erl
@@ -1,14 +1,17 @@
 -module(calc).
--export([rpn/1]).
+-export([rpn/1, calc/1]).
 
 rpn(String)->rpncalc_implementation(string:tokens(String, " ")).
+% calc(String)->rpncalc_implementation(parse_to_polish(string:tokens(String, " "))).
+calc(String)->parse_to_polish(string:tokens(String, " ")).
+
 
 rpncalc_implementation(Tokens) ->
     try lists:foldl(fun operate/2, [], Tokens) of
         [A] -> A;
         _ -> invalid_expression
     catch
-        _:_ -> invalid_expression
+        _ -> invalid_expression
     end.
 
 operate("+", [A, B | T]) -> [B + A | T];
@@ -21,9 +24,35 @@ operate("ln", [A | T]) -> [math:log(A) | T];
 operate("log10", [A | T]) -> [math:log10(A) | T];
 operate("sum", Stack) -> [lists:sum(Stack)];
 operate("prod", Stack) -> [lists:foldl(fun erlang:'*'/2, 1, Stack)];
-operate(Number, Stack) ->
-    try erlang:list_to_float(Number) of
-        Var -> [Var | Stack]
+operate(StringRepresentation, Stack) when is_list(StringRepresentation) -> [get_number(StringRepresentation) | Stack];
+operate(Number, Stack) when is_integer(Number) -> [Number | Stack].
+
+
+%% Transform to RPN
+
+plus_minus([]) -> [];
+plus_minus(["+" | Tokens]) -> plus_minus(Tokens) ++ ["+"];
+plus_minus(["-" | Tokens]) -> plus_minus(Tokens) ++ ["-"];
+plus_minus(Tokens) -> mul_div(Tokens).
+
+mul_div([]) -> [];
+mul_div(["*" | Tokens]) -> plus_minus(Tokens) ++ ["*"];
+mul_div(["/" | Tokens]) -> plus_minus(Tokens) ++ ["/"];
+mul_div(Tokens) -> symbols(Tokens).
+
+symbols([]) -> [];
+symbols(["(" | Tokens]) -> plus_minus(Tokens);
+symbols([")" | Tokens]) -> plus_minus(Tokens);
+symbols([StringRepresentation | Tokens]) -> [get_number(StringRepresentation) | plus_minus(Tokens)].
+
+parse_to_polish(Tokens) -> plus_minus(Tokens).
+
+
+%% Helper functions
+
+get_number(StringRepresentation) ->
+    try erlang:list_to_float(StringRepresentation) of
+        Var -> Var
     catch
-        _:_ -> [erlang:list_to_integer(Number) | Stack]
+        _:_ -> erlang:list_to_integer(StringRepresentation)
     end.
